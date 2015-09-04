b1dadfdd6d9910870d2b5aac72a4ba88a6659f26
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-09-04 17:50:39 -0400

    set up user registration

--- web/models/user.ex
+++ web/models/user.ex
@@ -5,12 +5,16 @@ defmodule Hello.User do
     field :name, :string
     field :email, :string
     field :bio, :string
+    field :crypted_password, :string
+    field :password, :string, virtual: true
+    
+    has_many :messags, Hello.Message
 
     timestamps
   end
 
-  @required_fields ~w(name email bio)
-  @optional_fields ~w()
+  @required_fields ~w(name email password)
+  @optional_fields ~w(bio)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -18,8 +22,11 @@ defmodule Hello.User do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
+
   def changeset(model, params \\ :empty) do
-    model
-    |> cast(params, @required_fields, @optional_fields)
-  end
+  model
+  |> cast(params, @required_fields, @optional_fields)
+  |> validate_format(:email, ~r/@/)
+  |> validate_length(:password, min: 5)
+end
 end
